ad30da532a0df9399805addf7785c6226618b640
melonchmp <asamalihamza@gmail.com>
2022-03-18 10:52:50 +0000

    Changed the desing a bit and added list items with line trhough crossed on click event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation";
-import { useState } from "react";
 import Home from "./Home";
 import Test from "./Test";
 
 function App() {
 
-  const [theme, setTheme] = useState('light');
   return (
     <div className="App">
       <div className="content">
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,17 @@
 const Home = () => {
+
+    const crossLine = (e) => {
+        const element = e.target;
+        element.classList.toggle("crossed-line");
+    };
+
     return ( 
         <div className="home-page">
-            <h2>LOREM IPSUM</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nostrum delectus voluptatibus atque adipisci. Quaerat consectetur ipsam alias tempora enim, beatae vitae minima velit nemo minus inventore explicabo, provident exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus commodi fugit aperiam laborum consectetur, et animi suscipit dolorum consequatur explicabo? Eius tenetur, non totam at distinctio est eveniet id necessitatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem qui veritatis temporibus nobis, laborum porro illum, consequatur dolore aut voluptatibus quod assumenda pariatur accusamus quia illo corrupti repellendus non? Quibusdam.</p>
+            <p onClick={crossLine}>Apples</p>
+            <p onClick={crossLine}>Bread</p>
+            <p onClick={crossLine}>Honey</p>
+            <p onClick={crossLine}>Milk</p>
+            <p onClick={crossLine}>Toast</p>
         </div>
      );
 }
